4d28559f6f86512c9f38d7e995ef04f1600db2d7
league <league@imac-22.attlocal.net>
2021-09-03 18:27:49 -0700

    Change The View League Invaders

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,31 @@
+//Token: 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
+
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
 
+public static final int WIDTH = 500;
+public static final int HEIGHT = 800;
+
+JFrame frame;
+GamePanel panel;
+
+final int MENU = 0;
+final int GAME = 1;
+final int END = 2;
+
+LeagueInvaders() {
+	//UI
+		frame = new JFrame();
+		panel = new GamePanel();
+}
+
+void setup() {
+	frame.add(panel);
+	
+	frame.setSize(WIDTH, HEIGHT);
+	frame.setVisible(true);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+}
 }
